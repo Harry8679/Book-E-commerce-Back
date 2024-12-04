b6e7226f89a18e61a0a0d3c1072965de5faa88e9
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-04 23:52:06 +0100

    user controller

---
+++ controllers/user.controller.js
@@ -0,0 +1,5 @@
+const test = (req, res) => {
+  res.send('Test');
+};
+
+module.exports = { test };
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,7 @@
 const express = require('express');
+const { test } = require('../controllers/user.controller');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Hello from user');
-});
+router.get('/', test);
 
 module.exports = router;
