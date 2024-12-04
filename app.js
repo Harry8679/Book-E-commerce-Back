93698439a75fef87fe83047a184ceddab0170f42
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-04 23:28:28 +0100

    DB Connected

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 app.get('/', (req, res) => {
@@ -7,6 +8,11 @@ app.get('/', (req, res) => {
 });
 
 const port = process.env.PORT || 8008;
+
+mongoose
+  .connect(process.env.MONGO_URI, {})
+  .then(() => console.log('DB Connected'))
+  .catch((err) => console.log('DB Error: ' + err));
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
