5559f0f9c2b94d5701e8b4362edad14ad6d9b02c
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-05 15:58:44 +0100

    validator middleware added in app.js

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { signup } = require('../controllers/user.controller');
+const { userSignupValidator } = require('../validator/index.validator');
 const router = express.Router();
 
-router.post('/signup', signup);
+router.post('/signup', userSignupValidator, signup);
 
 module.exports = router;
