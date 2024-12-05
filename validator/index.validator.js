9cf15515805b838e64faaceef49d12b52c2c886d
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-05 16:03:05 +0100

    index.validator.js updated

--- validator/index.validator.js
+++ validator/index.validator.js
@@ -1,13 +1,20 @@
-exports.userSignupValidator = (req, res, next) => {
-  req.check('name', 'Name is required').notEmpty();
-  req.check('email', 'Email must be between 3 to 32 characters long').matches(/.+\@.+\..+/).withMessage('Email must contain @').isLength({ min: 4, max: 32 });
-  req.check('password', 'Password is required').notEmpty();
-  req.check('password').isLength({ min: 6 }).withMessage('Password must contain at least 6 characters').matches(/\d/).withMessage('Password must contain a number');
+const { check, validationResult } = require('express-validator');
 
-  const errors = req.validationErrors();
-  if (errors) {
-    const firstError = errors.map(error => error.message)[0];
-    return res.status(400).json({ error: firstError });
-  }
-  next();
-};
+// Middleware pour valider les données de l'utilisateur
+exports.userSignupValidator = [
+  // Validation des champs
+  check('name', 'Name is required').notEmpty(),
+  check('email', 'Email must be between 3 to 32 characters long').isEmail().withMessage('Invalid email format').isLength({ min: 3, max: 32 }),
+  check('password', 'Password is required').notEmpty(),
+  check('password').isLength({ min: 6 }).withMessage('Password must contain at least 6 characters').matches(/\d/).withMessage('Password must contain a number'),
+
+  // Gestion des erreurs de validation
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const firstError = errors.array()[0].msg; // Récupérer le premier message d'erreur
+      return res.status(400).json({ error: firstError });
+    }
+    next();
+  },
+];
