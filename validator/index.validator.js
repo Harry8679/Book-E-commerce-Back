02772e274875ecf83c55252e9d338fd3ee4f4df3
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-05 16:14:44 +0100

    Validator updated for signup

--- validator/index.validator.js
+++ validator/index.validator.js
@@ -4,9 +4,14 @@ const { check, validationResult } = require('express-validator');
 exports.userSignupValidator = [
   // Validation des champs
   check('name', 'Name is required').notEmpty(),
-  check('email', 'Email must be between 3 to 32 characters long').isEmail().withMessage('Invalid email format').isLength({ min: 3, max: 32 }),
+  check('email')
+  .notEmpty().withMessage('Email is required') // Vérifie que l'email n'est pas vide
+    .isEmail().withMessage('Invalid email format') // Vérifie que c'est un email valide
+    .isLength({ min: 3, max: 32 }).withMessage('Email must be between 3 to 32 characters long'), // Vérifie la longueur de l'email
   check('password', 'Password is required').notEmpty(),
-  check('password').isLength({ min: 6 }).withMessage('Password must contain at least 6 characters').matches(/\d/).withMessage('Password must contain a number'),
+  check('password')
+    .isLength({ min: 6 }).withMessage('Password must contain at least 6 characters')
+    .matches(/\d/).withMessage('Password must contain a number'),
 
   // Gestion des erreurs de validation
   (req, res, next) => {
