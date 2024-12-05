55b1a47135ea23e21191c1e46177d2216dc874cd
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-05 13:30:38 +0100

    User Signup controller updated for registration

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,14 +1,21 @@
 const User = require('../models/user.model');
 
-const signup = (req, res) => {
-  console.log('req.body', req.body);
-  const user = new User(req.body);
-  user.save((err, user) => {
-    if (err) {
-      return res.status(400).json({ err });
-    }
-    res.json({ user });
-  });
+const signup = async (req, res) => {
+  try {
+    console.log('req.body', req.body);
+
+    // Création d'un nouvel utilisateur avec les données du corps de la requête
+    const user = new User(req.body);
+
+    // Sauvegarde de l'utilisateur dans la base de données
+    const savedUser = await user.save();
+
+    // Réponse avec les données de l'utilisateur sauvegardé
+    res.json({ user: savedUser });
+  } catch (err) {
+    // Gestion des erreurs
+    res.status(400).json({ err: err.message });
+  }
 };
 
 module.exports = { signup };
