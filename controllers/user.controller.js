f70abba670301aa7d9ded6bb8523bfeef40ea4c7
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-05 13:25:25 +0100

    User Signup controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,14 @@
+const User = require('../models/user.model');
+
 const signup = (req, res) => {
-  res.send('Sign up');
+  console.log('req.body', req.body);
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({ err });
+    }
+    res.json({ user });
+  });
 };
 
 module.exports = { signup };
