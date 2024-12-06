5eeba34eaa62181fb4d81c4b1fd69c8b5a27d5ec
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-07 00:00:00 +0100

    Useless code

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -3,7 +3,7 @@ const User = require('../models/user.model');
 
 // Middleware pour vérifier si l'utilisateur est connecté
 exports.requireSignin = expressjwt({
-  secret: process.env.JWT_SECRET, // Clé secrète pour vérifier le token
+  secret: process.env.JWT_SECRET,
   algorithms: ['HS256'], // Algorithme utilisé pour signer le token
   requestProperty: 'auth', // Ajouter les informations du token à req.auth
 });
