fb73e123b49e8b797b1530e8a9e4ebba0154c062
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-06 23:59:11 +0100

    Useless comment

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { signup, signin, signout, profile } = require('../controllers/auth.controller');
-const { requireSignin, userById } = require('../middlewares/auth.middleware'); // Import du middleware
+const { requireSignin, userById, isAuth } = require('../middlewares/auth.middleware');
 const router = express.Router();
 
 router.post('/signup', signup);
@@ -13,7 +13,7 @@ router.get('/protected', requireSignin, (req, res) => {
 });
 
 // Route pour obtenir le profil de l'utilisateur
-router.get('/secret/:userId', profile);
+router.get('/secret/:userId', requireSignin, isAuth, profile);
 
 // Middleware paramétrique pour récupérer un utilisateur par ID
 router.param('userId', userById);
