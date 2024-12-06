b8e2f38c7ed92759ded35dc071d54edc7ef6c530
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-06 07:03:01 +0100

    Middleware for checking is an user is logged or not

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,8 +1,8 @@
-const expressJwt = require('express-jwt');
+const { expressjwt } = require('express-jwt');
 
 // Middleware pour vérifier si l'utilisateur est connecté
-exports.requireSignin = expressJwt({
+exports.requireSignin = expressjwt({
   secret: process.env.JWT_SECRET, // Clé secrète pour vérifier le token
   algorithms: ['HS256'], // Algorithme utilisé pour signer le token
-  userProperty: 'auth', // Ajouter les informations du token à req.auth
+  requestProperty: 'auth', // Ajouter les informations du token à req.auth
 });
