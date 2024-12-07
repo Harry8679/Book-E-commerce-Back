5b7b694660391266bf50580aa0e2485d0133ce16
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-07 22:55:01 +0100

    Create a Category

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,5 +1,20 @@
-const create = (req, res) => {
-  res.send('Create a Category');
+const Category = require("../models/category.model");
+
+const create = async (req, res) => {
+  try {
+    // Création d'un nouvel utilisateur avec les données du corps de la requête
+    const category = new Category(req.body);
+
+    // Sauvegarde de l'utilisateur dans la base de données
+    const savedCategory = await category.save();
+
+    // Réponse avec les données de l'utilisateur sauvegardé
+    res.json({ category: savedCategory });
+  } catch (err) {
+    // Gestion des erreurs avec le helper
+    const message = errorHandler(err); 
+    res.status(400).json({ error: message });
+  }
 };
 
 module.exports = { create };
