79ccb4869de9a61800318297b8bf47ae81cc0f45
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-09 14:23:08 +0100

    Product controller 1

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,5 +1,18 @@
+const formidable = require('formidable');
+const _ = require('lodash');
+const Product = require('../models/product.model');
+
 const create = (req, res) => {
-  res.send('Product created successfully');
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: 'Image could not be uploaded'
+      })
+    }
+    let product = new Product(fields);
+  });
 }
 
 module.exports = { create };
