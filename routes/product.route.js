af3a5a434216b44e17c8cc6db32414e67587c359
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-09 13:40:43 +0100

    Product Route

---
+++ routes/product.route.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { create } = require('../controllers/category.controller');
+const { requireSignin, isAuth, isAdmin } = require('../middlewares/auth.middleware');
+const router = express.Router();
+
+router.post('/create/:userId', requireSignin, isAuth, isAdmin, create);
+
+module.exports = router;
