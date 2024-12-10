c5aad7daecb81cd30e441314301382909520684e
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-10 05:26:50 +0100

    Create from a product controller

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -6,29 +6,47 @@ const { errorHandler } = require('../helpers/dbErrorHandler.helper');
 
 const create = (req, res) => {
   let form = new formidable.IncomingForm();
-  form.keepExtensions = true;
+  form.keepExtensions = true; // Garde les extensions des fichiers
+
   form.parse(req, (err, fields, files) => {
     if (err) {
       return res.status(400).json({
-        error: 'Image could not be uploaded'
-      })
+        error: 'Image could not be uploaded',
+      });
     }
+
+    // Vérification des champs et des fichiers
     let product = new Product(fields);
 
     if (files.photo) {
-      product.photo.data = fs.readFileSync(files.photo.path);
-      product.photo.contentType = files.photo.type;
+      if (!files.photo.path) {
+        return res.status(400).json({
+          error: 'File path is missing in the uploaded photo',
+        });
+      }
+
+      // Lecture et sauvegarde de l'image
+      try {
+        product.photo.data = fs.readFileSync(files.photo.path);
+        product.photo.contentType = files.photo.type;
+      } catch (fileReadError) {
+        return res.status(400).json({
+          error: 'Error reading the uploaded photo',
+        });
+      }
     }
 
-    product.save((err, result) => {
-      if (err) {
+    // Sauvegarde du produit dans la base de données
+    product.save((saveErr, result) => {
+      if (saveErr) {
         return res.status(400).json({
-          error: errorHandler(err)
+          error: errorHandler(saveErr),
         });
       }
       res.json(result);
     });
   });
-}
+};
+
 
 module.exports = { create };
