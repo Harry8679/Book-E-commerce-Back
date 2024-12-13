c93d44f63e2e219b3a805596becb6a7f0cd5a632
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-13 11:50:49 +0100

    Product ID param

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { requireSignin, isAuth, isAdmin, userById } = require('../middlewares/auth.middleware');
-const { create } = require('../controllers/product.controller');
+const { create, productById } = require('../controllers/product.controller');
 const router = express.Router();
 
 router.post('/create/:userId', requireSignin, isAuth, isAdmin, create);
 
 // Middleware paramétrique pour userId
 router.param('userId', userById);
+router.param('productId', productById);
 
 module.exports = router;
