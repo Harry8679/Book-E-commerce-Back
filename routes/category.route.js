cff30f5a134e07e6111b2aaf68a189619bd3d2fd
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-18 21:56:08 +0100

    Category Route updated

--- routes/category.route.js
+++ routes/category.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { create, listCategories, getCategoryById } = require('../controllers/category.controller');
+const { create, listCategories, getCategoryById, updateCategory, deleteCategory } = require('../controllers/category.controller');
 const { requireSignin, isAuth, isAdmin, userById } = require('../middlewares/auth.middleware');
 const router = express.Router();
 
@@ -24,5 +24,7 @@ router.param('categoryId', async (req, res, next, id) => {
 router.post('/create/:userId', requireSignin, isAuth, isAdmin, create);
 router.get('/', listCategories); // Lister toutes les catégories
 router.get('/:categoryId', getCategoryById); // Récupérer une catégorie par ID
+router.put('/:categoryId/:userId', requireSignin, isAuth, isAdmin, updateCategory); // Modifier une catégorie
+router.delete('/:categoryId/:userId', requireSignin, isAuth, isAdmin, deleteCategory); // Supprimer une catégorie
 
 module.exports = router;
