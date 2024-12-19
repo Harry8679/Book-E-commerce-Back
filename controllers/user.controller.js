a1b71ce117e2448d362333e88a4bd4c85134b9d5
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-19 23:57:34 +0100

    Update controller updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -34,19 +34,31 @@ const read = async (req, res) => {
 
 
 const update = async (req, res) => {
-  User.findOneAndUpdate(
-    { _id: req.profile._id },
-    { $set: req.body },
-    { new: true },
-    (err, user) => {
-      if (err) {
-        return res.status(400).json({
-          error: 'You are not authorized to perform this action'
-        });
-      }
+  try {
+    // Recherche et mise à jour de l'utilisateur
+    const user = await User.findOneAndUpdate(
+      { _id: req.profile._id },
+      { $set: req.body },
+      { new: true }
+    );
+
+    // Si l'utilisateur n'est pas trouvé
+    if (!user) {
+      return res.status(404).json({
+        error: 'User not found',
+      });
     }
-  )
+
+    // Réponse avec les données mises à jour
+    res.json(user);
+  } catch (err) {
+    // Gestion des erreurs
+    res.status(400).json({
+      error: 'You are not authorized to perform this action',
+    });
+  }
 };
+
 
 
 module.exports = { userById, read, update };
