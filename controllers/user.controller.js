fcd6f2d4b1a858f87dc47295b56e9f239a15e7fb
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-19 23:47:27 +0100

    User By ID updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,15 +1,21 @@
 const User = require('../models/user.model');
 
-const userById = (req, res, next, id) => {
-  User.findById(id).exec((err, user) => {
-    if (err || !user) {
-      return res.status(400).json({
-        error : 'User not found'
-      });
+const userById = async (req, res, next, id) => {
+  try {
+    // Recherche de l'utilisateur par ID
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
     }
+
+    // Attacher le profil de l'utilisateur à la requête
     req.profile = user;
     next();
-  })
-}
+  } catch (err) {
+    // Gestion des erreurs
+    res.status(500).json({ error: 'Error fetching user' });
+  }
+};
 
-module.exports = { userById }
+module.exports = { userById };
