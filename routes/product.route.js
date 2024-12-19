5ce292c0faed1be249dc27538542625c66837a0a
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-19 21:33:04 +0100

    List Categories updated

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { requireSignin, isAuth, isAdmin, userById } = require('../middlewares/auth.middleware');
-const { create, productById, getAllProducts, getProductById, deleteProduct, updateProduct, list, listRelated } = require('../controllers/product.controller');
-const { listCategories } = require('../controllers/category.controller');
+const { create, productById, getAllProducts, getProductById, deleteProduct, updateProduct, list, listRelated, listCategories } = require('../controllers/product.controller');
 const router = express.Router();
 
 router.post('/create/:userId', requireSignin, isAuth, isAdmin, create);
+router.get('/categories', listCategories);
 router.get('/:productId', getProductById);
 // router.delete('/:productId/:userId', deleteProduct);
 router.delete('/:productId/:userId', requireSignin, isAuth, isAdmin, deleteProduct);
@@ -12,7 +12,6 @@ router.put('/:productId/:userId', requireSignin, isAuth, isAdmin, updateProduct)
 // router.get('/', getAllProducts);
 router.get('/', list);
 router.get('/related/:productId', listRelated);
-router.get('/categories', listCategories);
 
 // Middleware paramétrique pour userId
 router.param('userId', userById);
