8a8f79fc5f79fd41ef3916d6948c5752a6a9e435
Harry MacCode <emarh.harry.code@gmail.com>
2024-12-20 11:58:03 +0100

    Routes added for user

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,21 +1,16 @@
 const express = require('express');
-const { signup, signin, signout, profile } = require('../controllers/auth.controller');
-const { requireSignin, userById, isAuth } = require('../middlewares/auth.middleware');
+const { userById, read, update } = require('../controllers/user.controller');
+const { requireSignin, isAuth } = require('../middlewares/auth.middleware');
+
 const router = express.Router();
 
-router.post('/signup', signup);
-router.post('/signin', signin);
-router.get('/signout', signout);
-
-// Exemple de route protégée
-router.get('/protected', requireSignin, (req, res) => {
-  return res.json({ message: 'This route is protected' });
-});
-
-// Route pour obtenir le profil de l'utilisateur
-router.get('/secret/:userId', requireSignin, isAuth, profile);
-
-// Middleware paramétrique pour récupérer un utilisateur par ID
+// Middleware paramétrique pour charger l'utilisateur par ID
 router.param('userId', userById);
+
+// Route pour lire le profil utilisateur
+router.get('/profile/:userId', requireSignin, isAuth, read);
+
+// Route pour mettre à jour le profil utilisateur
+router.put('/profile/:userId', requireSignin, isAuth, update);
 
 module.exports = router;
