2fc0fb43afcf4c1ee73a80ea684d4581dcfcc93a
Harry MacCode <emarh.harry.code@gmail.com>
2025-01-23 23:38:42 +0100

    Order Route updated

--- routes/order.route.js
+++ routes/order.route.js
@@ -1,25 +1,21 @@
 const express = require('express');
 const { createOrder, updateOrderPayment, getUserOrders, paymentStripe } = require('../controllers/order.controller');
-const { requireSignin, isAuth, isAdmin, userById } = require('../middlewares/auth.middleware');
-const { orderById } = require('../middlewares/order.middleware'); // Middleware pour obtenir une commande par ID
+const { requireSignin, isAuth, userById } = require('../middlewares/auth.middleware');
 const router = express.Router();
 
 // Créer une commande (accessible uniquement aux utilisateurs authentifiés)
-// router.post('/create/:userId', requireSignin, isAuth, createOrder);
-router.post('/create/', requireSignin, isAuth, createOrder);
+router.post('/create', requireSignin, isAuth, createOrder);
 
-// Mettre à jour le paiement d'une commande (accessible uniquement aux utilisateurs authentifiés)
-router.put('/:orderId/pay/:userId', requireSignin, isAuth, updateOrderPayment);
+// Mettre à jour le paiement d'une commande
+router.put('/:orderId/pay', requireSignin, isAuth, updateOrderPayment);
 
-// Obtenir toutes les commandes d'un utilisateur (accessible uniquement aux utilisateurs authentifiés)
-router.get('/my-orders/:userId', requireSignin, isAuth, getUserOrders);
+// Obtenir toutes les commandes d'un utilisateur
+router.get('/my-orders', requireSignin, isAuth, getUserOrders);
 
+// Route pour les paiements Stripe
 router.post('/payments/stripe', requireSignin, isAuth, paymentStripe);
 
 // Middleware paramétrique pour `userId`
 router.param('userId', userById);
-
-// Middleware paramétrique pour `orderId`
-// router.param('orderId', orderById);
 
 module.exports = router;
