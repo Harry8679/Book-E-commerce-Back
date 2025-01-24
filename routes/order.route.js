656b26ff6c3c653ec6dfe220de5ea68a1fe58e52
Harry MacCode <emarh.harry.code@gmail.com>
2025-01-24 03:05:51 +0100

    Get All Order route

--- routes/order.route.js
+++ routes/order.route.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { createOrder, updateOrderPayment, getUserOrders, paymentStripe } = require('../controllers/order.controller');
+const { createOrder, updateOrderPayment, getUserOrders, paymentStripe, getAllOrders } = require('../controllers/order.controller');
 const { requireSignin, isAuth, userById } = require('../middlewares/auth.middleware');
 const router = express.Router();
 
 // Créer une commande (accessible uniquement aux utilisateurs authentifiés)
 router.post('/create', requireSignin, isAuth, createOrder);
+
+// Obtenir toutes les commandes (réservé aux administrateurs)
+router.get('/all-orders', requireSignin, isAuth, isAdmin, getAllOrders);
 
 // Mettre à jour le paiement d'une commande
 router.put('/:orderId/pay', requireSignin, isAuth, updateOrderPayment);
