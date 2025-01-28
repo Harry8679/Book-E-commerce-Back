cb625ad0b61bebeffdb091cb7ae0effefa62c9fb
Harry MacCode <emarh.harry.code@gmail.com>
2025-01-28 19:12:45 +0100

    Comment Route updated

---
+++ routes/comment.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { requireSignin, isAuth } = require('../middlewares/auth.middleware');
+const { createComment, getCommentsByProduct } = require('../controllers/comment.controller');
+
+const router = express.Router();
+
+// Ajouter un commentaire (seulement si l'utilisateur a acheté le produit)
+router.post('/product/:productId/comment', requireSignin, isAuth, createComment);
+
+// Obtenir tous les commentaires d'un produit
+router.get('/product/:productId/comments', getCommentsByProduct);
+
+module.exports = router;
