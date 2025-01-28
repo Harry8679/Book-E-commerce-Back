4c930a3758adb1580d65f14e1ce7a33e12fc737a
Harry MacCode <emarh.harry.code@gmail.com>
2025-01-28 18:58:19 +0100

    Product Model updated

--- models/product.model.js
+++ models/product.model.js
@@ -1,45 +1,52 @@
 const mongoose = require('mongoose');
 const { ObjectId } = mongoose.Schema;
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: [true, 'Name is required'],
-    maxlength: [32, 'Name cannot exceed 32 characters'],
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, 'Name is required'],
+      maxlength: [32, 'Name cannot exceed 32 characters'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Description is required'],
+      maxlength: [2000, 'Description cannot exceed 2000 characters'],
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+    shipping: {
+      type: Boolean,
+      required: false,
+    },
+    comments: [
+      {
+        type: ObjectId,
+        ref: 'Comment', // Référence aux commentaires liés à ce produit
+      },
+    ],
   },
-  description: {
-    type: String,
-    required: [true, 'Description is required'],
-    maxlength: [2000, 'Description cannot exceed 2000 characters'],
-  },
-  price: {
-    type: Number,
-    trim: true,
-    required: [true, 'Price is required'],
-    maxlength: [32, 'Price cannot exceed 32 characters'],
-  },
-  category: {
-    type: ObjectId,
-    ref: 'Category',
-    required: [true, 'Category is required'],
-    maxlength: [32, 'Category cannot exceed 32 characters'],
-  },
-  quantity: {
-    type: Number,
-  },
-  sold: {
-    type: String,
-    default: 0
-  },
-  photo: {
-    data: Buffer,
-    contentType: String
-  },
-  shipping: {
-    type: Boolean,
-    required: [false, 'Category is required'],
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Product', productSchema);
